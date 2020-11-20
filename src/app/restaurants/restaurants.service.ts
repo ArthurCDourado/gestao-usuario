209fb548a048316a3f2d549115a4b2e9985881d0
arthur <arthurdourado01@hotmail.com>
2020-11-19 21:01:40 -0300

    Reviews Component

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import { Http } from '@angular/http';
 import { MEAT_API } from 'app/app.api';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
 import { Restaurant } from './restaurant/restaurant.model';
+import {ErrorHandler} from '../app.error-handler';
 
 @Injectable()
 export class RestaurantsService {
@@ -11,7 +12,14 @@ export class RestaurantsService {
   constructor(private http: Http) { }
 
   restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
+    return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json()).catch(ErrorHandler.handleError);
   }
 
+  restaurantById(id: string): Observable<Restaurant> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}`).map(response => response.json()).catch(ErrorHandler.handleError);
+  }
+
+  reviewsOfRestaurant(id: string): Observable<any> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`).map(response => response.json()).catch(ErrorHandler.handleError);
+  }
 }
